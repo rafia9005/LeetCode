aa716fa32670afdee6917001ac418a8cb748fdec
rafia9005 <rafia9005@gmail.com>
2026-05-04 18:39:13 +0700

    Add LeetCode problem solutions: Roman to Integer, Longest Common Prefix, Add Two Numbers, and Palindrome Number

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_list = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        for i in range(len(s)):
+            value = roman_list[s[i]]
+            if i + 1 < len(s) and value < roman_list[s[i + 1]]:
+                total -= value
+            else:
+                total += value
+        return total
